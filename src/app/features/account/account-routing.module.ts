74cd116ca30dcd075725b01cec64d2366b5a5515
alsiesta <as@daslizenzatelier.de>
2023-10-17 00:53:42 +0200

    work i progress - edit profile details

--- src/app/features/account/account-routing.module.ts
+++ src/app/features/account/account-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from 'src/app/shared/layout/layout.component';
 
 import { AccountPageComponent } from './account-page/account-page.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 const routes: Routes = [
   {
@@ -10,6 +13,27 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: 'profile', component: AccountPageComponent },
+    ]
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'edit-profile', component: EditProfileComponent },
+    ]
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'details', component: ProfileDetailsComponent },
+    ]
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'changepassword', component: ChangePasswordComponent },
     ]
   }
 ];
--- src/app/features/account/account.module.ts
+++ src/app/features/account/account.module.ts
@@ -6,6 +6,7 @@ import { AccountPageComponent } from './account-page/account-page.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   imports: [
@@ -13,7 +14,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     SharedModule,
     AccountRoutingModule
   ],
-  declarations: [AccountPageComponent, ChangePasswordComponent, ProfileDetailsComponent],
+  declarations: [AccountPageComponent, ChangePasswordComponent, ProfileDetailsComponent, EditProfileComponent],
   exports: [AccountPageComponent]
 })
 export class AccountModule { }
